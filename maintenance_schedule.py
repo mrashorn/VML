3213002cc207b48226ce47652c9a1d32580e4421
Matt Ashorn <matthew.ashorn@utexas.edu>
2022-08-19 11:56:54 -0500

    Created the first revision of the default maintenance schedule.

--- maintenance_schedule.py
+++ maintenance_schedule.py
@@ -5,4 +5,20 @@ class Maintenance_Schedule:
 
     def __init__(self):
         """Initialze the maintenance schedule."""
-        self.oil_change_interval = 5000
+        self.oil_interval = 5000
+        self.tire_rotation_interval = 7500
+        self.engine_air_filter_interval = 30000
+        self.cabin_air_filter_interval = 30000
+        self.fuel_filter_interval = 50000 
+        self.battery_interval = 75000
+        self.brake_fluid_interval = 30000
+        self.brake_pads_interval = 50000
+        self.brake_rotor_interval = 60000
+        self.radiator_coolant = 60000
+        self.transmission_fluid_interval = 60000
+        self.vehicle_hoses_interval = 90000
+        self.power_steering_fluid_interval = 75000
+        self.spark_plugs_interval = 100000
+        self.timing_belt_interval = 90000
+        self.rear_differential_fluid_interval = 60000
+
